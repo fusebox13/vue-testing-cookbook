0607f616f14c21afdd9f9ed07e438c6fc9a5fe1c
Kunle Oshiyoye <mgen.glder@gmail.com>
2019-02-19 22:35:47 -0500

    Add Slider element component to list of components - Also add titles to example components

--- src/views/Element.vue
+++ src/views/Element.vue
@@ -1,16 +1,39 @@
 <template>
   <div class="element">
-    <ElButtonExample/>
+    <div class="element-component">
+      <div class="element-title">
+        Element Button
+      </div>
+        <ElButtonExample/>
+    </div>
+
+    <div class="element-component">
+      <div class="element-title">
+        Element Slider
+        </div>
+        <ElSliderExample/>
+      </div>
   </div>
 </template>
 
 <script>
 import ElButtonExample from '@/components/element/ElButtonExample';
+import ElSliderExample from '@/components/element/ElSliderExample';
 
 export default {
   name: "elementio",
   components: {
-    ElButtonExample
+    ElButtonExample,
+    ElSliderExample
   }
 };
 </script>
+<style lang="scss" scoped>
+.element-title {
+  font-size: 2em;
+  color: green;
+}
+.element-component {
+  margin-bottom: 80px
+}
+</style>
