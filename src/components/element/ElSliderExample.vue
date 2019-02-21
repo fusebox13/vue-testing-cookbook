6509851caf58d143f825572acc4caeb368ff5922
Kunle Oshiyoye <mgen.glder@gmail.com>
2019-02-20 19:18:10 -0500

    Add Slider element component to list of components (#1)
    - Also add titles to example components

---
+++ src/components/element/ElSliderExample.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="hello">
+        <el-slider v-model="sliderValue"></el-slider>
+    </div>
+</template>
+<script>
+export default {
+    name: "ElSliderExample",
+    data() {
+        return {
+            sliderValue: 10
+        }
+    }
+}
+</script>
